0a1d0256c126af85d919afa261048fec45f8e504
scq000 <scq000@hotmail.com>
2017-03-01 19:11:05 +0800

    format code

--- Set/Set.js
+++ Set/Set.js
@@ -3,72 +3,133 @@
  * @author: scq000
  */
 function Set(arr) {
-	this.items = {};
-	if(arr && (arr instanceof Array)) {
-		arr.forEach(function(item) {
-			this.items[JSON.stringify(item)] = item;
-		}, this);
-	}
+    this.items = {};
+    if (arr && (arr instanceof Array)) {
+        arr.forEach(function(item) {
+            this.items[JSON.stringify(item)] = item;
+        }, this);
+    }
 }
 
 Set.prototype = {
-	has: function(value) {
-		return this.items.hasOwnProperty(JSON.stringify(value));
-	},
+    has: function(value) {
+        return this.items.hasOwnProperty(JSON.stringify(value));
+    },
 
-	add: function(value) {
-		if(!this.has(value)) {
-			this.items[JSON.stringify(value)] = value;
-			return true;
-		}
-		return false;
-	},
+    add: function(value) {
+        if (!this.has(value)) {
+            this.items[JSON.stringify(value)] = value;
+            return true;
+        }
+        return false;
+    },
 
-	delete: function(value) {
-		if(this.has(value)) {
-			delete this.items[JSON.stringify(value)];
-			return true;
-		}
-		return false;
-	},
+    delete: function(value) {
+        if (this.has(value)) {
+            delete this.items[JSON.stringify(value)];
+            return true;
+        }
+        return false;
+    },
 
-	addMany: function(values) {
-		for(var i = 0; i < values.length; i++) {
-			this.add(values[i]);
-		}
-	},
+    addMany: function(values) {
+        for (var i = 0; i < values.length; i++) {
+            this.add(values[i]);
+        }
+    },
 
-	deleteMany: function(values) {
-		for(var i = 0; i < values.length; i++) {
-			this.delete(values[i]);
-		}
-	},
+    deleteMany: function(values) {
+        for (var i = 0; i < values.length; i++) {
+            this.delete(values[i]);
+        }
+    },
 
-	clear: function() {
-		this.items = {};
-	},
-	//根据条件过滤
-	filter: function(fn) {
-		for(var key in this.items) {
-			if(!fn(this.items[key])) {
-				this.delete(this.items[key]);
-			}
-		}
-		return new Set(this.items);
-	},
+    clear: function() {
+        this.items = {};
+    },
+    //根据条件过滤
+    filter: function(fn) {
+        for (var key in this.items) {
+            if (!fn(this.items[key])) {
+                this.delete(this.items[key]);
+            }
+        }
+        return new Set(this.items);
+    },
 
-	size: function() {
-		return Object.keys(this.items).length;
-	},
+    size: function() {
+        return Object.keys(this.items).length;
+    },
 
-	//返回由value组成的数组
-	values: function() {
-		var result = [];
+    //返回由value组成的数组
+    values: function() {
+        var result = [];
 
-		for(var key in this.items) {
-			result.push(this.items[key]);
-		}
+        for (var key in this.items) {
+            result.push(this.items[key]);
+        }
 
-		return result;
-	}
+        return result;
+    }
+}
+Set.prototype = {
+    has: function(value) {
+        return this.items.hasOwnProperty(JSON.stringify(value));
+    },
+
+    add: function(value) {
+        if (!this.has(value)) {
+            this.items[JSON.stringify(value)] = value;
+            return true;
+        }
+        return false;
+    },
+
+    delete: function(value) {
+        if (this.has(value)) {
+            delete this.items[JSON.stringify(value)];
+            return true;
+        }
+        return false;
+    },
+
+    addMany: function(values) {
+        for (var i = 0; i < values.length; i++) {
+            this.add(values[i]);
+        }
+    },
+
+    deleteMany: function(values) {
+        for (var i = 0; i < values.length; i++) {
+            this.delete(values[i]);
+        }
+    },
+
+    clear: function() {
+        this.items = {};
+    },
+    //根据条件过滤
+    filter: function(fn) {
+        for (var key in this.items) {
+            if (!fn(this.items[key])) {
+                this.delete(this.items[key]);
+            }
+        }
+        return new Set(this.items);
+    },
+
+    size: function() {
+        return Object.keys(this.items).length;
+    },
+
+    //返回由value组成的数组
+    values: function() {
+        var result = [];
+
+        for (var key in this.items) {
+            result.push(this.items[key]);
+        }
+
+        return result;
+    }
 }
