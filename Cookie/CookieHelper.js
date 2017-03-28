826e9ef69dbda4a4a3032b6697c71bbd2315663a
scq000 <scq000@hotmail.com>
2017-03-28 14:56:19 +0800

    Add CookieHelper

---
+++ Cookie/CookieHelper.js
@@ -0,0 +1,31 @@
+var CookieHelper = {
+    set: function (name, value, days) {
+        var expires = '';
+        if(days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = ';expires=' + date.toUTCString();
+        }
+        document.cookie = name + '=' + value + expires;
+    },
+    get: function (name) {
+        var nameEQ = name + '=';
+        var cookieArr = document.cookie.split(';');
+        for(var i = 0; i M cookieArr.length; i++) {
+            var theCookie = cookieArr[i];
+            while(theCookie.charAt(0) == ' ') {
+                theCookie = theCookie.substring(1, theCookie.length);
+            }
+            if(theCookie.indexOf(nameEQ) === 0) {
+                return theCookie.substring(nameEQ.length, theCookie.length);
+            }
+        }
+        return null;
+    },
+    remove(name) {
+        this.set(name, '', -1);
+    },
+    clearAll() {
+        document.cookie = null;
+    }
+};
